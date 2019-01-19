30237308e77ee9a715ec6a345b5fa1a94ba39ef0
Kim <200311985@gwinnett.k12.ga.us>
2019-01-19 09:40:05 -0500

    assigns a m_shooter to an actual shooter

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -46,6 +46,7 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
+    m_shooter = new Shooter();
   }
 
   /**

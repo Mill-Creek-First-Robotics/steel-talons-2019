453f4c4b61f12113d782f0453647f62f8b2a549c
Kim <200311985@gwinnett.k12.ga.us>
2019-01-19 10:18:47 -0500

    single -> double solenoid pt 2

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,11 +1,12 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 
 public class Shooter extends Subsystem {
-    Solenoid pitchSolenoid;
+    DoubleSolenoid pitchSolenoid;
     public Shooter(){
         pitchSolenoid = new DoubleSolenoid(RobotMap.SOLONOID,RobotMap.NULLONOID);
     }

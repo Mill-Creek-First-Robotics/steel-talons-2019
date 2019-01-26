53b6b7d02665d3d2dfd9cb4106a1107082d6e509
SteelTalonRoboticsLaptop <frc6910@gmail.com>
2019-01-26 11:05:13 -0500

    FIxed incosistency in class names

--- src/main/java/frc/robot/subsystems/PIDSubsystem.java
+++ src/main/java/frc/robot/subsystems/PIDSubsystem.java
@@ -12,10 +12,10 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class ExampleSubsystem extends Subsystem {
+public class PIDSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-
+  
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.

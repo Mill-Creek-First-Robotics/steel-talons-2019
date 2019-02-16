e809dab7ad12c13c3c1a4e7913a93d56ee18f4e8
GPIOctapus <sdj2016@gmail.com>
2019-02-16 11:39:49 -0500

    Changed Pneumatics

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -8,24 +8,24 @@ public class Shooter extends Subsystem {
     Solenoid pitchSolenoid1;
     Solenoid pitchSolenoid2;
     Solenoid pitchSolenoid3;
-    Solenoid pitchSolenoid4;
     public Shooter(){
         pitchSolenoid1 = new Solenoid(RobotMap.SOLONOID_1);
         pitchSolenoid2 = new Solenoid(RobotMap.SOLONOID_2);
         pitchSolenoid3 = new Solenoid(RobotMap.SOLONOID_3);
-        pitchSolenoid4 = new Solenoid(RobotMap.SOLONOID_4);
     }
     public void pitchUp(){
         pitchSolenoid1.set(true);
         pitchSolenoid2.set(true);
-        pitchSolenoid3.set(true);
-        pitchSolenoid4.set(true);
     }
     public void pitchDown(){
         pitchSolenoid1.set(false);
         pitchSolenoid2.set(false);
+    }
+    public void armOut(){
+        pitchSolenoid3.set(true);
+    }
+    public void armIn(){
         pitchSolenoid3.set(false);
-        pitchSolenoid4.set(false);
     }
     
 

466ea1c3aecddb69fc3a0dbd7319c8fbcddb6af7
SteelTalonRoboticsLaptop <frc6910@gmail.com>
2019-03-16 10:59:06 -0400

    Fixed class name

--- src/main/java/frc/robot/commands/BallRetract.java
+++ src/main/java/frc/robot/commands/BallRetract.java
@@ -13,8 +13,8 @@ import frc.robot.Robot;
 /**
  * An example command.  You can replace me with your own command.
  */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class BallRetract extends Command {
+  public BallRetract() {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.m_subsystem);
   }
